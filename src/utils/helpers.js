11504bcf92016c5a068165c019ab1e5877128e45
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-09-10 18:17:26 +0200

    format price

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,13 @@
-export const formatPrice = () => {}
+export const formatPrice = (price) => {
+    // return new Intl.NumberFormat('en-US', {
+    //     style: 'currency',
+    //     currency: 'USD'
+    // }).format(price/100)
+    
+    return (price / 100).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    })
+}
 
 export const getUniqueValues = () => {}
