89dbe037e831050964b02d7fee0aedf3abf3140d
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-09-10 22:28:25 +0200

    enable changing the main image with right and left arrows

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,15 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const ProductImages = ({ images }) => {
-  let [mainImg, setMainImg] = useState(images[0]);
+  let [mainImg, setMainImg] = useState(images[2]);
+
+  function handlekeydown(e) {
+    if (e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
+      // get index of mianImg
+      let index = images.indexOf(mainImg);
+
+      // change it based on arrow
+      if (e.key === 'ArrowRight') {
+        index++;
+        if (index > images.length - 1) {
+          index = 0;
+        }
+      }
+      if (e.key === 'ArrowLeft') {
+        index--;
+        if (index < 0) {
+          index = images.length - 1;
+        }
+      }
+      // change mainImg
+      setMainImg(images[index]);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handlekeydown);
+
+    return () => {
+      document.removeEventListener('keydown', handlekeydown);
+    };
+  });
 
   return (
     <Wrapper>
       <img src={mainImg.url} alt="main" className="main" />
       <div className="gallery">
         {images.map((image) => {
-          let {id, url, filename} = image
+          let { id, url, filename } = image;
           return (
             <img
               key={id}
