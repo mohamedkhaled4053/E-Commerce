809aa513b1eb892d9d8af18602816149df5b5a97
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-09-12 17:30:07 +0200

    no products matched the search

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,13 +4,19 @@ import GridView from './GridView';
 import ListView from './ListView';
 
 const ProductList = () => {
-  let {filteredProducts :products, layout} = useFilterContext()
-  if(layout === 'list-view'){
-    return <ListView products={products} />
+  let { filteredProducts: products, layout } = useFilterContext();
+  if (products.length < 1) {
+    return (
+      <h5 style={{textTransform: "none"}}>
+        Sorry, no products matched your search...
+      </h5>
+    );
   }
-  return (
-    <GridView products={products}/>
-  );
+
+  if (layout === 'list-view') {
+    return <ListView products={products} />;
+  }
+  return <GridView products={products} />;
 };
 
 export default ProductList;
