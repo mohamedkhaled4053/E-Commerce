4ce771566193627049346e399db0d2fcade9c494
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-09-14 15:32:16 +0200

    handle empty cart

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -5,9 +5,24 @@ import { Link } from 'react-router-dom';
 import { CartContent, PageHero } from '../components';
 
 const CartPage = () => {
+  let { cart } = useCartContext();
+
+  if (cart.length < 1) {
+    return (
+      <Wrapper className="page-100">
+        <div className="empty">
+          <h2>Your cart is empty</h2>
+          <Link className="btn" to="/products">
+            fill it
+          </Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
   return (
     <main>
-      <PageHero title='cart'/>
+      <PageHero title="cart" />
       <Wrapper className="page">
         <CartContent />
       </Wrapper>
