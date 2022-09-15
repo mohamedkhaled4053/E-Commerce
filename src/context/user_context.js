4a6f332f18700d79ed07865da500686b0602b0ba
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-09-15 17:31:02 +0200

    setup authentication

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -3,6 +3,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 const UserContext = React.createContext()
 export const UserProvider = ({ children }) => {
+  
   return (
     <UserContext.Provider value='user context'>{children}</UserContext.Provider>
   )
--- src/index.js
+++ src/index.js
@@ -5,16 +5,22 @@ import App from './App';
 import { ProductsProvider } from './context/products_context';
 import { FilterProvider } from './context/filter_context';
 import { CartProvider } from './context/cart_context';
-import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev-cgypzy4n.us.auth0.com"
+    clientId="qWjHQS27YQJT4c7hMxMppwWRxEQGVO7V"
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 );
