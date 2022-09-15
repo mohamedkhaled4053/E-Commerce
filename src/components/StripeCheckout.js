f1a1628d407f4fc69a32d7a1d8305ea8911f8724
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-09-15 21:54:08 +0200

    add static code to checkout page

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -1,20 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { loadStripe } from '@stripe/stripe-js';
-import {
-  CardElement,
-  useStripe,
-  Elements,
-  useElements,
-} from '@stripe/react-stripe-js';
-import axios from 'axios';
 import { useCartContext } from '../context/cart_context';
 import { useUserContext } from '../context/user_context';
 import { formatPrice } from '../utils/helpers';
 import { useHistory } from 'react-router-dom';
+import {
+  FaCcAmex,
+  FaCcDiscover,
+  FaCcMastercard,
+  FaCcVisa,
+} from 'react-icons/fa';
 
 const CheckoutForm = () => {
-  return <h4>hello from Stripe Checkout </h4>;
+  return (
+    <div className="row">
+      <div className="col-75">
+        <div className="container">
+          <article>
+            <h4>Hello, محمد خالد</h4>
+            <p>Your total is $30.99</p>
+            <p>Test Card Number: 4242 4242 4242 4242</p>
+          </article>
+          <form>
+
+         
+                <h3>Payment</h3>
+                <label for="fname">Accepted Cards</label>
+                <div className="icon-container">
+                  <FaCcVisa style={{ color: ' navy' }} />
+                  <FaCcAmex style={{ color: ' blue' }} />
+                  <FaCcMastercard style={{ color: ' red' }} />
+                  <FaCcDiscover style={{ color: ' orange' }} />
+                </div>
+                <label for="cname">Name on Card</label>
+                <input
+                  type="text"
+                  id="cname"
+                  name="cardname"
+                  placeholder="John More Doe"
+                />
+                <label for="ccnum">Credit card number</label>
+                <input
+                  type="text"
+                  id="ccnum"
+                  name="cardnumber"
+                  placeholder="1111-2222-3333-4444"
+                />
+                <label for="expmonth">Exp Month</label>
+                <input
+                  type="text"
+                  id="expmonth"
+                  name="expmonth"
+                  placeholder="September"
+                />
+
+                <input type="text" name="" id="" style={{ display: 'none' }} />
+
+                <div className="row">
+                  <div className="col-50">
+                    <label for="expyear">Exp Year</label>
+                    <input
+                      type="text"
+                      id="expyear"
+                      name="expyear"
+                      placeholder="2018"
+                    />
+                  </div>
+                  <div className="col-50">
+                    <label for="cvv">CVV</label>
+                    <input type="text" id="cvv" name="cvv" placeholder="352" />
+                  </div>
+                </div>
+            
+   
+            <input type="submit" value="Pay" className="btn" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const StripeCheckout = () => {
@@ -26,141 +90,85 @@ const StripeCheckout = () => {
 };
 
 const Wrapper = styled.section`
-  form {
-    width: 30vw;
-    align-self: center;
-    box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1),
-      0px 2px 5px 0px rgba(50, 50, 93, 0.1),
-      0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
-    border-radius: 7px;
-    padding: 40px;
+  padding: 5em 0;
+
+  .row {
+    display: -ms-flexbox; /* IE10 */
+    display: flex;
+    -ms-flex-wrap: wrap; /* IE10 */
+    flex-wrap: wrap;
+    margin: 0 -16px;
   }
-  input {
-    border-radius: 6px;
-    margin-bottom: 6px;
-    padding: 12px;
-    border: 1px solid rgba(50, 50, 93, 0.1);
-    max-height: 44px;
-    font-size: 16px;
+
+  .col-50 {
+    -ms-flex: 50%; /* IE10 */
+    flex: 50%;
+  }
+
+  .col-75 {
+    -ms-flex: 75%; /* IE10 */
+    flex: 75%;
+  }
+
+  .col-50,
+  .col-75 {
+    padding: 0 16px;
+  }
+
+  .container {
+    background-color: #f2f2f2;
+    padding: 5px 20px 15px 20px;
+    border: 1px solid lightgrey;
+    border-radius: 3px;
+  }
+
+  input[type='text'] {
     width: 100%;
-    background: white;
-    box-sizing: border-box;
-  }
-  .result-message {
-    line-height: 22px;
-    font-size: 16px;
-  }
-  .result-message a {
-    color: rgb(89, 111, 214);
-    font-weight: 600;
-    text-decoration: none;
-  }
-  .hidden {
-    display: none;
-  }
-  #card-error {
-    color: rgb(105, 115, 134);
-    font-size: 16px;
-    line-height: 20px;
-    margin-top: 12px;
-    text-align: center;
-  }
-  #card-element {
-    border-radius: 4px 4px 0 0;
+    margin-bottom: 20px;
     padding: 12px;
-    border: 1px solid rgba(50, 50, 93, 0.1);
-    max-height: 44px;
-    width: 100%;
-    background: white;
-    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 3px;
   }
-  #payment-request-button {
-    margin-bottom: 32px;
-  }
-  /* Buttons and links */
-  button {
-    background: #5469d4;
-    font-family: Arial, sans-serif;
-    color: #ffffff;
-    border-radius: 0 0 4px 4px;
-    border: 0;
-    padding: 12px 16px;
-    font-size: 16px;
-    font-weight: 600;
-    cursor: pointer;
+
+  label {
+    margin-bottom: 10px;
     display: block;
-    transition: all 0.2s ease;
-    box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
+  }
+
+  .icon-container {
+    padding: 7px 0;
+    font-size: 24px;
+  }
+
+  svg {
+    margin-right: 10px;
+  }
+
+  .btn {
+    background-color: #04aa6d;
+    color: white;
+    padding: 12px;
+    margin: 10px 0;
+    border: none;
     width: 100%;
+    border-radius: 3px;
+    cursor: pointer;
+    font-size: 17px;
   }
-  button:hover {
-    filter: contrast(115%);
+
+  .btn:hover {
+    background-color: #45a049;
   }
-  button:disabled {
-    opacity: 0.5;
-    cursor: default;
+
+  span.price {
+    float: right;
+    color: grey;
   }
-  /* spinner/processing state, errors */
-  .spinner,
-  .spinner:before,
-  .spinner:after {
-    border-radius: 50%;
-  }
-  .spinner {
-    color: #ffffff;
-    font-size: 22px;
-    text-indent: -99999px;
-    margin: 0px auto;
-    position: relative;
-    width: 20px;
-    height: 20px;
-    box-shadow: inset 0 0 0 2px;
-    -webkit-transform: translateZ(0);
-    -ms-transform: translateZ(0);
-    transform: translateZ(0);
-  }
-  .spinner:before,
-  .spinner:after {
-    position: absolute;
-    content: '';
-  }
-  .spinner:before {
-    width: 10.4px;
-    height: 20.4px;
-    background: #5469d4;
-    border-radius: 20.4px 0 0 20.4px;
-    top: -0.2px;
-    left: -0.2px;
-    -webkit-transform-origin: 10.4px 10.2px;
-    transform-origin: 10.4px 10.2px;
-    -webkit-animation: loading 2s infinite ease 1.5s;
-    animation: loading 2s infinite ease 1.5s;
-  }
-  .spinner:after {
-    width: 10.4px;
-    height: 10.2px;
-    background: #5469d4;
-    border-radius: 0 10.2px 10.2px 0;
-    top: -0.1px;
-    left: 10.2px;
-    -webkit-transform-origin: 0px 10.2px;
-    transform-origin: 0px 10.2px;
-    -webkit-animation: loading 2s infinite ease;
-    animation: loading 2s infinite ease;
-  }
-  @keyframes loading {
-    0% {
-      -webkit-transform: rotate(0deg);
-      transform: rotate(0deg);
-    }
-    100% {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
-  }
-  @media only screen and (max-width: 600px) {
-    form {
-      width: 80vw;
+
+  /* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other (and change the direction - make the "cart" column go on top) */
+  @media (max-width: 800px) {
+    .row {
+      flex-direction: column-reverse;
     }
   }
 `;
--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero, StripeCheckout } from '../components'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero, StripeCheckout } from '../components';
 // extra imports
-import { useCartContext } from '../context/cart_context'
-import { Link } from 'react-router-dom'
+import { useCartContext } from '../context/cart_context';
+import { Link } from 'react-router-dom';
 
 const CheckoutPage = () => {
-  return <h4>checkout page</h4>
-}
-const Wrapper = styled.div``
-export default CheckoutPage
+  return (
+    <main>
+      <PageHero title="checkout" />
+      <Wrapper className='page'>
+        <StripeCheckout />
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .empty {
+    text-align: center;
+  }
+`;
+export default CheckoutPage;
