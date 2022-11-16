a6f6ba9891ca547038561081c963c69ef5fd19f7
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-11-17 00:25:08 +0200

    fix a typo

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-const PageHero = ({ title, prodcut }) => {
+const PageHero = ({ title, product }) => {
   return (
     <Wrapper>
       <div className="section-center">
         <h3>
           <Link to="/">Home </Link>{' '}
-          {prodcut && <Link to="/products">/ prodcuts</Link>}/ {title}
+          {product && <Link to="/products">/ products</Link>}/ {title}
         </h3>
       </div>
     </Wrapper>
